583263ad5c345a26c10957fd75c23614aa68704b
xuzheng <1580181628@qq.com>
2021-06-25 01:38:10 +0800

    东方财富-简单的股票爬虫程序1.2

--- src/main/java/com/stock/spider/config/ThreadPoolConfig.java
+++ src/main/java/com/stock/spider/config/ThreadPoolConfig.java
@@ -15,17 +15,23 @@ public class ThreadPoolConfig {
     @Bean
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        //获取当前系统cpu核心数
         int cpu = Runtime.getRuntime().availableProcessors();
-        //设置线程池参数信息
+        //核心线程数: 线程池初始化后的初始数量
         taskExecutor.setCorePoolSize(cpu);
+        //最大线程数: 超过核心线程数+阻塞队列满了 则新建线程直到达到最大线程数
         taskExecutor.setMaxPoolSize(cpu * 2);
-        taskExecutor.setQueueCapacity(300);
+        //阻塞队列长度: 超过核心线程数就将任务放进队列中
+        taskExecutor.setQueueCapacity(cpu * 3);
+        //过期时间: 超过核心线程数的线程如果闲置时间超过这个数 则被销毁
         taskExecutor.setKeepAliveSeconds(60);
+        //线程名称前缀
         taskExecutor.setThreadNamePrefix("stock-spider-");
+        //优雅的关闭线程 全部线程都执行完任务后才关闭
         taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        //修改拒绝策略为使用当前线程执行
+        //任务数量如果超过 最大线程数+阻塞队列长度 则表示线程池满了 执行这个饱和策略 即使用当前线程执行 比如 main线程
         taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-        //初始化线程池
+        //初始化
         taskExecutor.initialize();
         return taskExecutor;
     }
--- src/main/java/com/stock/spider/service/impl/StockServiceImpl.java
+++ src/main/java/com/stock/spider/service/impl/StockServiceImpl.java
@@ -2,7 +2,6 @@ package com.stock.spider.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
 import com.stock.spider.service.StockService;
 import com.stock.spider.utils.WebUtil;
 import org.springframework.core.task.TaskExecutor;
@@ -24,10 +23,17 @@ public class StockServiceImpl implements StockService {
     @Resource
     WebUtil webUtil;
 
+    private final String fields1 = "f1,f2,f3,f4,f5,f6";
+    private final String fields2 = "f53,f57";
+    private final String klt = "101";
+    private final String fqt = "1";
+    private final String beg = "0";
+    private final String end = "20500000";
+
     @Override
     public void stock(String industryCode) {
         String stockApi = "http://80.push2.eastmoney.com/api/qt/clist/get?pn=1&pz=500&po=1&np=1&fltt=2&invt=2&fid=f3&fs=b:" + industryCode + "+f:!50&fields=f12,f14";
-        String sizeApi = "http://push2.eastmoney.com/api/qt/stock/get?fltt=2&invt=2&secid=%s.%s&fields=f43,f47,f168";
+        String stockKLineApi = "https://push2his.eastmoney.com/api/qt/stock/kline/get?fields1=%s&fields2=%s&klt=%s&fqt=%s&secid=%s.%s&beg=%s&end=%s";
 
         Map<Double, String> concurrentHashMap = new ConcurrentHashMap<>();
 
@@ -49,22 +55,28 @@ public class StockServiceImpl implements StockService {
                         type = "0";
                     }
 
-                    String formatApi = String.format(sizeApi, type, code);
-                    String sizeWeb = webUtil.getWeb(formatApi);
-                    JSONObject sizeObject = JSON.parseObject(sizeWeb).getJSONObject("data");
-                    String f43 = sizeObject.get("f43").toString();
-                    String f47 = sizeObject.get("f47").toString();
-                    String f168 = sizeObject.get("f168").toString();
+                    Double sumPrice = 0.0;//元
+                    Double avgPrice;//元
+                    Double sumAmount = 0.0;//亿
+                    Double avgAmount;//亿
 
-                    double price = Double.parseDouble(f43);//股票价格
-                    if (price > 10) {
-                        value.append(",").append(price);
-                        int turnover = Integer.parseInt(f47);//成交量(1手=100股)
-                        double rate = Double.parseDouble(f168);//换手率
-                        double size = turnover * 100 / rate / 100 / 10000;//股票盘子大小(流通股)(单位亿)
-                        Double key = new BigDecimal(size).setScale(2, RoundingMode.HALF_UP).doubleValue();//保留两位小数
+                    String formatApi = String.format(stockKLineApi, fields1, fields2, klt, fqt, type, code, beg, end);
+                    String stockWeb = webUtil.getWeb(formatApi);
+                    JSONArray stockArray = JSON.parseObject(stockWeb).getJSONObject("data").getJSONArray("klines");
+                    int size = stockArray.size();
+                    int limit = size > 40 ? size - 40 : 0;//2个月 1个月20个交易日
+                    for (int i = size - 1; i >= limit; i--) {
+                        String[] split = stockArray.get(i).toString().split(",");
+                        String price = split[0];
+                        String amount = split[1];
+                        sumPrice += Double.parseDouble(price);
+                        sumAmount += Double.parseDouble(amount);
+                    }
 
-                        concurrentHashMap.put(key, value.toString());
+                    avgPrice = new BigDecimal(sumPrice / 40.0).setScale(2, RoundingMode.HALF_UP).doubleValue();
+                    avgAmount = new BigDecimal(sumAmount / 40.0 / 1e8).setScale(2, RoundingMode.HALF_UP).doubleValue();
+                    if (avgPrice > 10 && avgAmount > 2) {
+                        concurrentHashMap.put(avgAmount, value.toString());
                     }
                 }
                 countDownLatch.countDown();
@@ -77,7 +89,7 @@ public class StockServiceImpl implements StockService {
             e.printStackTrace();
         }
 
-        //按股票盘子大小顺序输出
+        //股票最近2个月的平均交易额 顺序输出
         System.out.println("-----分界线-----");
         Map<Double, String> result = new LinkedHashMap<>();
         concurrentHashMap.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(e -> result.put(e.getKey(), e.getValue()));
