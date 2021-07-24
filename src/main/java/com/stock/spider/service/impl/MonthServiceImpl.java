492ab66cb01a545f5c215572bd221d80bf920a62
xuzheng <1580181628@qq.com>
2021-07-24 22:51:03 +0800

    2021.七八月

--- src/main/java/com/stock/spider/service/impl/MonthServiceImpl.java
+++ src/main/java/com/stock/spider/service/impl/MonthServiceImpl.java
@@ -17,19 +17,19 @@ public class MonthServiceImpl implements MonthService {
     public List<Map<String, String>> month() {
         List<Map<String, String>> result = new ArrayList<>();
 
-        Map<String, String> industrySortedMap_7 = this.month("-7");
+        Map<String, String> industrySortedMap_current = this.month("-7");
 
-        Map<String, String> industrySortedMap_8 = this.month("-8");
+        Map<String, String> industrySortedMap_next = this.month("-8");
 
         //求交集
         redisUtil.selectDataBase(0);
-        Map<String, String> intersection = industrySortedMap_7.entrySet().stream()
-                .filter(e -> industrySortedMap_8.entrySet().stream().map(Map.Entry::getKey)
+        Map<String, String> intersection = industrySortedMap_current.entrySet().stream()
+                .filter(e -> industrySortedMap_next.entrySet().stream().map(Map.Entry::getKey)
                         .collect(Collectors.toList()).contains(e.getKey()))
                 .collect(Collectors.toMap(e -> e.getKey(), e -> redisUtil.getValueByString(e.getKey())));
 
-        result.add(industrySortedMap_7);
-        result.add(industrySortedMap_8);
+        result.add(industrySortedMap_current);
+        result.add(industrySortedMap_next);
         result.add(intersection);
 
         return result;
