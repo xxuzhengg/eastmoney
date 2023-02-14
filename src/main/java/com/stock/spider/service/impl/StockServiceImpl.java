b0ad77feef27201b1ffb606ccb86d0198133c710
xuzheng <1580181628@qq.com>
2023-02-14 14:45:25 +0800

    东方财富-简单的股票爬虫程序1.6

--- src/main/java/com/stock/spider/service/impl/StockServiceImpl.java
+++ src/main/java/com/stock/spider/service/impl/StockServiceImpl.java
@@ -17,7 +17,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class StockServiceImpl implements StockService {
